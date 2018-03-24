98383cce78781842b22bdc969b187f8da3c83076
Krystian Czaplicki <krystian@Krystians-MacBook-Pro.local>
2018-03-24 19:10:20 +0100

    Init commit

---
+++ XamarinFormsWebViewSample/LoginFormDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace XamarinFormsWebViewSample
+{
+    public class LoginFormDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ XamarinFormsWebViewSample/XamarinFormsWebViewSamplePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using XLabs.Forms.Controls;
+using XLabs.Serialization.JsonNET;
+
+namespace XamarinFormsWebViewSample
+{
+    public partial class XamarinFormsWebViewSamplePage : ContentPage
+    {
+        private const string WebViewUrl = "http://krystianczaplicki.com/webview/";
+        private const string LoginCallback = "login";
+        private const string DisplayLoginAlertFunctioName = "displayLoginAlert";
+        private const string Username = "User";
+
+        public XamarinFormsWebViewSamplePage()
+        {
+            InitializeComponent();
+
+            var serializer = new JsonSerializer();
+            var webView = new HybridWebView(serializer)
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Margin = new Thickness(20, 30),
+                Uri = new Uri(WebViewUrl)
+            };
+
+            webView.RegisterCallback(LoginCallback, (arg) =>
+            {
+                var loginForm = serializer.Deserialize<LoginFormDto>(arg);
+                var result = loginForm.Username == Username;
+                webView.CallJsFunction(DisplayLoginAlertFunctioName, new[] { result.ToString() });
+            });
+
+            Content = webView;
+        }
+    }
+}
